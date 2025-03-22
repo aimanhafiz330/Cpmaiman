83b2f97f4acbcc6835218a36c3b3e27d43f86048
aimanhafiz330 <aimanhafiz12567@gmail.com>
2025-03-22 11:33:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from pystyle import Colors, Colorate
 from pystyle import Center
 import datetime
 
-from cpmaimann import Cpmaiman
+from Cpmaimann import cpmaiman
 
 __CHANNEL_USERNAME__ = "car tfc"
 __GROUP_USERNAME__   = "car tfc_chat"
@@ -144,7 +144,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = HadjiMedia(acc_access_key)
+        cpm = cpmaiman(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
